e9ff2dba7280145db5ff8a16f2c3629915f35d95
yaaroslaw <yaaroslaw@gmail.com>
2017-04-24 19:03:58 +0300

    Added config for self hosting.

--- WebAPISelfHost/Program.cs
+++ WebAPISelfHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Http.SelfHost;
 
 namespace WebAPISelfHost
 {
@@ -10,7 +11,13 @@ namespace WebAPISelfHost
     {
         static void Main(string[] args)
         {
-
+            var config = new HttpSelfHostConfiguration("http://localhost:8999");
+            var server = new HttpSelfHostServer(config);
+           
+            var task = server.OpenAsync();
+            task.Wait();
+            Console.WriteLine("Server is up and runnig...");
+            Console.ReadLine();
         }
     }
 }
