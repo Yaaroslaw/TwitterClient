14b20ecd72d9e1e70a296188b7f4c0bc279cd14e
yaaroslaw <yaaroslaw@gmail.com>
2017-04-25 15:46:42 +0300

    Added authorization to Self Hosting

--- WebAPISelfHost/MyConfig.cs
+++ WebAPISelfHost/MyConfig.cs
@@ -1,10 +1,24 @@
-﻿namespace WebAPISelfHost
+﻿using System.Web.Http.SelfHost;
+using System.Web.Http.SelfHost.Channels;
+using System.ServiceModel;
+
+
+namespace WebAPISelfHost
 {
-    public  class MyConfig
+    public  class MyConfig : HttpSelfHostConfiguration
     {
         private string v;
+        protected override System.ServiceModel.Channels.BindingParameterCollection OnConfigureBinding(HttpBinding httpBinding)
+        {
+            httpBinding.Security.Mode = HttpBindingSecurityMode.TransportCredentialOnly;
+            httpBinding.
+                Security.
+                Transport.
+                ClientCredentialType = HttpClientCredentialType.Windows;
+            return base.OnConfigureBinding(httpBinding);
+        }
 
-        public MyConfig(string v)
+        public MyConfig(string v) : base(v)
         {
             this.v = v;
         }
