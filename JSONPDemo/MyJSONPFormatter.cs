eab3b5787c6e46deb60414f43eb4a48836be455e
yaaroslaw <yaaroslaw@gmail.com>
2017-04-25 16:27:13 +0300

    Implemented MyJSONPFormatter

--- JSONPDemo/MyJSONPFormatter.cs
+++ JSONPDemo/MyJSONPFormatter.cs
@@ -1,4 +1,10 @@
-﻿using System.Net.Http.Formatting;
+﻿using JSONPDemo.Controllers;
+using System;
+using System.Net.Http.Formatting;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace JSONPDemo
 {
@@ -6,6 +12,27 @@ namespace JSONPDemo
     {
         public MyJSONPFormatter()
         {
+            this.SupportedMediaTypes.Add(new System.Net.Http.Headers.MediaTypeHeaderValue("application/javascript"));
+        }
+
+        public override bool CanReadType(Type type)
+        {
+            return false;
+        }
+
+        public override bool CanWriteType(Type type)
+        {
+            return type == (typeof(JSONPReturn));
+        }
+        public override Task WriteToStreamAsync(Type type, object value, Stream writeStream, HttpContent content, TransportContext transportContext)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                var jsonp = (JSONPReturn)value;
+                var sw = new StreamWriter(writeStream);
+                sw.Write("{0}({1})", jsonp.Callback, jsonp.JSON);
+                sw.Flush();
+            });
         }
     }
 }
