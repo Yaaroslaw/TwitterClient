10ea16dc044cf768b21f972d0eb2979944d51626
yaaroslaw <yaaroslaw@gmail.com>
2017-04-25 14:29:05 +0300

    Added server call with client

--- WebAPISelfHost/Program.cs
+++ WebAPISelfHost/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.SelfHost;
 
@@ -19,7 +20,17 @@ namespace WebAPISelfHost
             var task = server.OpenAsync();
             task.Wait();
             Console.WriteLine("Server is up and runnig...");
+            Console.WriteLine("Hit enter to call server with client");
             Console.ReadLine();
+            var client = new HttpClient(server);
+            client.GetAsync("http://localhost:8999/api/my").ContinueWith((t)=> 
+            {
+                var result = t.Result;
+                result.Content.ReadAsStringAsync().ContinueWith((readTask) =>
+                {
+                    Console.WriteLine("Client got response " + readTask.Result);
+                });
+            });
         }
     }
 }
